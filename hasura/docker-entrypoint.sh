719e6c0d702a95efd5c0a88bc187b9fcb310ce1a
Mark Sinclair <mmsinclair@users.noreply.github.com>
2022-10-19 17:54:30 +0100

    typo

--- hasura/docker-entrypoint.sh
+++ hasura/docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 npm install --global hasura-cli
 
 cd hasura/metadata
-hasura metadata apply --endpoint http://hasura:8080 --skip-update-check --admin-secret HASURA_ADMIN_SECRET
+hasura metadata apply --endpoint http://hasura:8080 --skip-update-check --admin-secret $HASURA_ADMIN_SECRET
